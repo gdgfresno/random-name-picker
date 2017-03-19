fc2f291c683f6a7d006221b11061951b9c48b584
MrCsabaToth <csaba.toth.us@gmail.com>
2017-03-19 08:56:33 -0700

    Sort the participants lexically, case insensitive

--- js/hat.js
+++ js/hat.js
@@ -33,19 +33,31 @@ function maskEmail(email) {
   var hatData = [];
   var restData = [];
 
-  $.each(participants, function(index, data) {
-    var tableData = [
-        data.name,
-        maskEmail(data.email),
-        data.generalSurvey,
-        data.sessionSurvey
-      ];
-    if (!data.generalSurvey || data.sessionSurvey < 1) {
-      restData.push(tableData);
-    } else {
-      hatData.push(tableData);
+  $.each(
+    participants.sort(
+      function(a, b) {
+        var nameA = a.name.toLowerCase(), nameB = b.name.toLowerCase();
+        if (nameA < nameB) //sort string ascending
+          return -1;
+        if (nameA > nameB)
+          return 1;
+        return 0; //default return value (no sorting)
+      }
+    ),
+    function (index, data) {
+      var tableData = [
+          data.name,
+          maskEmail(data.email),
+          data.generalSurvey,
+          data.sessionSurvey
+        ];
+      if (!data.generalSurvey || data.sessionSurvey < 1) {
+        restData.push(tableData);
+      } else {
+        hatData.push(tableData);
+      }
     }
-  });
+  );
 
   document.hatTable = $("#hat-table").DataTable({
     data: hatData
